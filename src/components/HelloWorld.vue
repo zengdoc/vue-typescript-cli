7c05cf1c482240905568846d1a447d450c6fbae0
zengdongchao <dongchao.zeng@gmail.com>
2020-03-23 09:10:10 +0800

    优化

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h1>{{ title }}</h1>
+    <h2>{{ subtitle }}</h2>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
@@ -33,28 +34,44 @@
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator';
+  import { Component, Provide, Vue } from 'vue-property-decorator';
+  import { getModule } from 'vuex-module-decorators';
+  import Example from '@/store/example';
 
-@Component
-export default class HelloWorld extends Vue {
-  @Prop() private msg!: string;
-}
+  const exampleModule = getModule(Example);
+
+  @Component
+  export default class HelloWorld extends Vue {
+      get title() {
+          return this.content.title;
+      }
+
+      get subtitle() {
+          return this.content.subtitle;
+      }
+
+      @Provide()
+      content: ContentType = {
+          title: 'Hello World',
+          subtitle: exampleModule.message,
+      };
+  }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="less">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  h3 {
+    margin: 40px 0 0;
+  }
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+  a {
+    color: #42b983;
+  }
 </style>
---
+++ src/global.d.ts
@@ -0,0 +1,5 @@
+interface ContentType {
+    title: string;
+    subtitle: string;
+}
+
